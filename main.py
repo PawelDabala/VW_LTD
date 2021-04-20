c0bde47b55ed05d7940e3a026f638c95efcc0525
Pawel Dabala <pdabala@gmial.com>
2021-04-20 06:28:06 +0200

    set method get_category_list

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ import pprint
 class VW_LCV:
     def __init__(self):
         page = get('https://vwdostawcze-sklep.pl')
-        self.bs = BeautifulSoup(page, "html.parser")
+        self.bs = BeautifulSoup(page.content, "html.parser")
 
-        self.set_category(self.bs)
+        self.set_spider(self.bs)
 
-    def set_category(self, bs):
-        category_list = self.get_category_list(bs)
+    def set_spider(self, bs):
+        categories = self.get_category_list(bs)
 
     def get_category_list(self, bs):
-        pass
+        category_links = bs.find('ul', class_="category-list swiper-wrapper").find_all("a")
+        categories = []
+        for category_link in category_links:
+            categories.append({'link': category_link['href'],
+                               'text': category_link.get_text("|", strip=True)
+                               })
+        return categories
+
+
+VW_LCV()
