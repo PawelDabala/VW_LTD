e843f28a7019ee2e4817c7feaf9037f54b33a653
Pawel Dabala <pdabala@gmial.com>
2021-04-22 16:39:20 +0200

    get all products links

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ class VW_LCV:
     page_address = 'https://vwdostawcze-sklep.pl'
 
     def __init__(self):
+        self.final_data = []
         page = get(self.page_address)
         self.bs = BeautifulSoup(page.content, "html.parser")
-
         self.set_spider(self.bs)
-        self.final_data = []
 
     def set_spider(self, bs):
         categories = self.get_category_list(bs)
@@ -35,18 +34,19 @@ class VW_LCV:
             subproduct_address = self.page_address + category['link']
             subproducts_page = get(subproduct_address)
             bs_subproducts_list = BeautifulSoup(subproducts_page.content, "html.parser")
-            self.get_products(bs_subproducts_list, category['name'])
+            self.final_data.extend(self.get_products(bs_subproducts_list, category['name']))
+        pass
 
     def get_products(self, subproducts, name):
 
         finish = True
-        nr = 0
         products = []
-        while finish and nr <= 5:
-            # todo function to check all records have status finish
+        while finish:
             if len(products) == 0:
                 # make new dictinary
                 alists = subproducts.find_all('a', class_="product-list__item")
+                if len(alists) == 0:
+                    continue
                 price = True if alists[0].find("div", class_="product-list__price") else False
                 if price:
                     # "no more subcategories"
@@ -56,7 +56,7 @@ class VW_LCV:
                                          'path': alist['href'],
                                          'finish': True
                                          })
-                    finish = True
+
                 else:
                     for alist in alists:
                         products.append({'product_name': name,
@@ -71,27 +71,37 @@ class VW_LCV:
                     path = get(self.page_address + product['path'])
                     products_bs = BeautifulSoup(path.content, "html.parser")
                     alists = products_bs.find_all('a', class_="product-list__item")
+                    if len(alists) == 0:
+                        continue
                     price = True if alists[0].find("div", class_="product-list__price") else False
 
                     if price:
                         # "no more subcategories"
-                        # do zmiany produkt powinien dodawać się do nowej zmiennej
                         for alist in alists:
-                            product['products'].append(f'{alist.find("p").get_text()}')
-                            product['path'] = alist['href']
-                            product['finish'] = True
-                            temp_products.append(product)
-                        finish = True
+                            temp_product = {}
+                            products_list = product['products'].copy()
+                            products_list.append(f'{alist.find("p").get_text()}')
+                            temp_product['product_name'] = product['product_name']
+                            temp_product['products'] = products_list
+                            temp_product['path'] = alist['href']
+                            temp_product['finish'] = True
+                            temp_products.append(temp_product)
+
                     else:
                         for alist in alists:
-                            product['products'].append(f'{alist.find("p").get_text()}')
-                            product['path'] = alist['href']
-                            product['finish'] = False
-                            temp_products.append(product)
+                            temp_product = {}
+                            products_list = product['products'].copy()
+                            products_list.append(f'{alist.find("p").get_text()}')
+                            temp_product['product_name'] = product['product_name']
+                            temp_product['products'] = products_list
+                            temp_product['path'] = alist['href']
+                            temp_product['finish'] = False
+                            temp_products.append(temp_product)
 
                 products = temp_products
-            print(nr)
-            nr += 1
+
+            finish = not (all([dic['finish'] for dic in products]))
+        return products
 
 
 VW_LCV()
