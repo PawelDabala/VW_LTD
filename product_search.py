0ca3a8ee206912e4f8d31ca906f679f1d5f5924a
Pawel Dabala <pdabala@gmial.com>
2021-04-26 10:59:04 +0200

    set google category for nr and last category

--- product_search.py
+++ product_search.py
@@ -12,19 +12,24 @@ def get_data():
 class FindProduct:
     def __init__(self, rows):
         self.rows = rows
+        self.row = None
 
     def get_product(self, product: str):
         for row in self.rows:
             if product.upper().strip() in row[0].upper().strip():
-                return row
-        else:
-            return None
+                self.row =  row
+                break
 
+    def get_google_nr(self):
+        return self.row[1] if self.row is not None else None
 
+    def get_last_category_google(self):
+        return self.row[-1] if self.row is not None else None
+    
 
 
 rows = get_data()
 
 find_product = FindProduct(rows)
-product = find_product.get_product('do kotwic')
-print(product)
+find_product.get_product('do kotwic')
+pass
